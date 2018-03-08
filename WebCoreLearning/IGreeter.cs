a1d6ddda035689ec48d93884e10747489b13278c
Mike K <mike.keena@thekeenas.com>
2018-03-08 15:34:00 -0600

    Through Serving Files

---
+++ WebCoreLearning/IGreeter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WebCoreLearning {
+	public interface IGreeter {
+		string GetMessageOfTheDay();
+	}
+	public class Greeter : IGreeter {
+		private IConfiguration _configuration;
+
+		public Greeter(IConfiguration configuration) {
+			_configuration = configuration;
+		}
+		public string GetMessageOfTheDay() {
+			return _configuration["Greeting"];
+		}
+	}
+}
